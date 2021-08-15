cc066c59bac63ac3204a6e553d4389b5df15e96a
saurabhgulab <41892363+saurabhgulab@users.noreply.github.com>
2021-08-15 23:18:39 +0530

    main file
    number%3===0 >> fizz
    number%5===0 >> Buzz
    both 3 & 5 >> fizzBuzz

---
+++ index.js
@@ -0,0 +1,21 @@
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+    while(count<=100){
+    if(count%3===0 && count%5===0){
+        output.push("fizzBuzz");
+    }
+    else if(count%3===0){
+        output.push("fizz");
+    } else if(count%5===0){
+        output.push("Buzz");
+    } else{
+        output.push(count);
+    }
+    count=count+1;
+}
+console.log(output);
+}
+
+fizzBuzz();
